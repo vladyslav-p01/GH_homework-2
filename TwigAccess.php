6212af9c08f170060887fb0b2950212128992b7b
Vladyslav <vladyslav.p01@gmail.com>
2015-10-20 22:40:35 +0300

    changing codestyle

--- TwigAccess.php
+++ TwigAccess.php
@@ -1,10 +1,9 @@
 <?php
-
 require_once 'vendor/autoload.php';
 
 Class TwigAccess
 {
-    public static function twigRender($template,$variables = array())
+    public static function twigRender($template, $variables = array())
     {
         $loader = new Twig_Loader_Filesystem('templates/');
         $twig = new Twig_Environment($loader);
--- github.php
+++ github.php
@@ -4,16 +4,16 @@ require_once "TwigAccess.php";
 
 use Guzzle\Http\Client;
 
-if (isset($_GET['user']) && $_GET['user']!=''){
+if (isset($_GET['user']) && $_GET['user'] != '') {
     $client = new Client("https://api.github.com");
     $request = $client->get('/users/'.$_GET['user']);
-    $url=$request->getUrl();
+    $url = $request->getUrl();
     $response = $request->send();
 
-    $answer=json_decode($response->getBody(),true);
-    echo TwigAccess::twigRender('github.html.twig',array(
-        'url'=>$url,
-        'data'=>$answer
+    $answer = json_decode($response->getBody(), true);
+    echo TwigAccess::twigRender('github.html.twig', array(
+        'url' => $url,
+        'data' => $answer
     ));
 } else {
     echo TwigAccess::twigRender('error.html.twig');
